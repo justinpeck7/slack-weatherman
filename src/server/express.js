5d9caa9c7a21970a5dd401a152fd3b75709526eb
Justin Peck <justinpeck7@gmail.com>
2021-02-28 17:48:20 -0600

    add basic logs ui

--- src/server/express.js
+++ src/server/express.js
@@ -1,14 +1,29 @@
 import express from "express";
 import logs from "./api/logs";
 import startScheduledTasks from "./scheduled-tasks";
+import cors from "cors";
+import path from "path";
 
 export const startServer = () => {
   startScheduledTasks();
 
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
   app.use("/api/logs", logs);
+
+  app.get("/logs", (req, res, next) => {
+    res.sendFile(
+      "index.html",
+      { root: path.resolve(__dirname, "../client/") },
+      (err) => {
+        if (err) {
+          next(err);
+        }
+      }
+    );
+  });
 
   app.listen(8080);
 };
