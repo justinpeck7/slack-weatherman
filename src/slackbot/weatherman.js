08750f4d425f3597c947bd96dcf1e2d1a3a9a968
justin <justinpeck7@gmail.com>
2023-01-21 17:23:39 -0600

    Update logging

--- src/slackbot/weatherman.js
+++ src/slackbot/weatherman.js
@@ -4,6 +4,23 @@ import installPlugins from "./install-plugins.js";
 import { SocketModeClient } from "@slack/socket-mode";
 import { WebClient } from "@slack/web-api";
 
+const logger = {
+  debug(...msgs) {
+    WeathermanDAO.log("debug: " + JSON.stringify(msgs));
+  },
+  info(...msgs) {
+    WeathermanDAO.log("info: " + JSON.stringify(msgs));
+  },
+  warn(...msgs) {
+    WeathermanDAO.log("warn: " + JSON.stringify(msgs));
+  },
+  error(...msgs) {
+    WeathermanDAO.log("error: " + JSON.stringify(msgs));
+  },
+  setLevel() {},
+  setName() {},
+};
+
 const appToken = process.env.SLACK_APP_TOKEN;
 const botToken = process.env.BOT_TOKEN;
 
@@ -11,11 +28,20 @@ const webClient = new WebClient(botToken);
 
 const socketClient = new SocketModeClient({
   appToken,
+  logger,
   autoReconnectEnabled: true,
+});
+
+socketClient.on("connecting", () => {
+  WeathermanDAO.log("Connecting...");
 });
 
 socketClient.on("connected", () => {
   WeathermanDAO.log("Weatherman Connected");
+});
+
+socketClient.on("disconnecting", () => {
+  WeathermanDAO.log("Disconnecting");
 });
 
 socketClient.on("disconnected", () => {
@@ -24,6 +50,14 @@ socketClient.on("disconnected", () => {
 
 socketClient.on("reconnecting", () => {
   WeathermanDAO.log("Reconnecting...");
+});
+
+socketClient.on("error", (error) => {
+  WeathermanDAO.log(`Error: ${JSON.stringify(error)}`);
+});
+
+socketClient.on("unable_to_socket_mode_start", (error) => {
+  WeathermanDAO.log(`unable_to_socket_mode_start: ${JSON.stringify(error)}`);
 });
 
 socketClient.on("message", async ({ event, ack }) => {
